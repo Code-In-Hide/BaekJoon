b58f092e2fb8add11ae75187f17e6382aec883f1
DongKyu Ryu <fbehdrb97@naver.com>
2023-09-21 17:32:24 +0900

    Q2741 Hello World! 출력 문제

---
+++ BaekJoon/src/Qize/Q2741/Main.java
@@ -0,0 +1,12 @@
+package Qize.Q2741;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner b = new Scanner(System.in);
+		int a = b.nextInt();
+		
+		for (int i = 1; i <= a; i++);
+	}
+}
