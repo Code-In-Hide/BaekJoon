cc210264543825a02b3b064066a788b2728e5684
DongKyu Ryu <fbehdrb97@naver.com>
2023-09-22 09:53:59 +0900

    Qize2741 for 문 N 찍기문제 풀이

--- BaekJoon/src/Qize/Q2741/Main.java
+++ BaekJoon/src/Qize/Q2741/Main.java
@@ -7,8 +7,8 @@ public class Main {
 		Scanner b = new Scanner(System.in);
 		int a = b.nextInt();
 		int c = 0;
-		
-		for(int i=1; i <= a; i++) {
+
+		for (int i = 1; i <= a; i++) {
 			c = i;
 			System.out.println(c);
 		}
