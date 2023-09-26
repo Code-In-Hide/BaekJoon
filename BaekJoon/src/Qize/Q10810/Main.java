2a9472e6df9d8a79a312b0131a9d34a3d4697bb1
DongKyu Ryu <fbehdrb97@naver.com>
2023-09-26 17:28:11 +0900

    백준 공넣기 문제 주석 설명 추가

--- BaekJoon/src/Qize/Q10810/Main.java
+++ BaekJoon/src/Qize/Q10810/Main.java
@@ -8,21 +8,19 @@ public class Main {
 		int N = sc.nextInt(); // 5
 		int M = sc.nextInt(); // 4
 
-		int[] b = new int[N];
-		for (int i = 0; i < M; i++) {
+		int[] b = new int[N]; // 배열 개수 N개 생성
+		for (int i = 0; i < M; i++) { // i 가 M보다 작을때까지 I,J,K 입력
 			int I = sc.nextInt();
 			int J = sc.nextInt();
 			int K = sc.nextInt();
 
-			for (int x = I - 1; x <= J - 1; x++) {
-				b[x] = K;
+			for (int x = I - 1; x <= J - 1; x++) { // I~J까지 범위 정할때
+				b[x] = K; // I~J까지 정한 범위에 K값 입력
 			}
 		}
-		for (int y = 0; y < b.length; y++) {
+		sc.close();
+		for (int y = 0; y < b.length; y++) { // 배열 개수만큼 출력
 			System.out.print(b[y] + " ");
 		}
-		System.out.println();
-
-		sc.close();
 	}
 }
