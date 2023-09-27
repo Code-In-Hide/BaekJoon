bea7639b737bb39e7f505e2f1753ea3c7978a648
DongKyu Ryu <fbehdrb97@naver.com>
2023-09-27 16:29:56 +0900

    별찍기2

---
+++ BaekJoon/src/Qize/Q2439/Main.java
@@ -0,0 +1,21 @@
+package Qize.Q2439;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		sc.close();
+
+		for (int i = 0; i < N; i++) {
+			for (int k = 0; k < N - 1 - i; k++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+}
