f5d7871e64713f2a6a9698695ed83ba73fcf0cc9
DongKyu Ryu <fbehdrb97@naver.com>
2023-10-13 17:40:34 +0900

    별찍기 뒤집힌피라미드

---
+++ BaekJoon/src/Qize/Q2443/Main.java
@@ -0,0 +1,21 @@
+package Qize.Q2443;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int inp = sc.nextInt();
+		sc.close();
+
+		for (int i = 0; i <= inp; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 1; k <= 2*(inp-i)-1; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
